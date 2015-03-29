8bff81bddc3bb34ef44a4719cc0cf6a069bedf40
Justin <justin@JustindeMacBook-Pro.local>
2015-03-29 22:33:12 +0800

    empty

---
+++ empty/empty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  empty
+//
+//  Created by Justin on 15/3/29.
+//  Copyright (c) 2015年 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ empty/empty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  empty
+//
+//  Created by Justin on 15/3/29.
+//  Copyright (c) 2015年 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
